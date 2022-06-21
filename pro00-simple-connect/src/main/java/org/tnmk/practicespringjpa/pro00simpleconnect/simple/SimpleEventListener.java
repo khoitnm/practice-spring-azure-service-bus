b060e32e1f804b0bd2eb95cd89f56a1009917dc8
Khoi Tran <khoi.tnm@gmail.com>
2022-06-21 15:30:03 -0400

    Convert message payload to object.

--- pro00-simple-connect/src/main/java/org/tnmk/practicespringjpa/pro00simpleconnect/simple/SimpleEventListener.java
+++ pro00-simple-connect/src/main/java/org/tnmk/practicespringjpa/pro00simpleconnect/simple/SimpleEventListener.java
@@ -1,16 +1,36 @@
 package org.tnmk.practicespringjpa.pro00simpleconnect.simple;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+import org.tnmk.practicespringjpa.pro00simpleconnect.simple.event.Event;
+
+import java.io.UnsupportedEncodingException;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class SimpleEventListener {
+
+  private final ObjectMapper objectMapper;
 
   @JmsListener(destination = "${simple.topic}", containerFactory = "topicJmsListenerContainerFactory",
       subscription = "${simple.subscription}")
-  public void receiveMessage(String message) {
-    log.info("Received message: {}", message);
+  public void receiveEvent(Message<Object> message) throws UnsupportedEncodingException, JsonProcessingException {
+    log.info("Received event as bytes: {}", message);
+    String json = toText((byte[]) message.getPayload());
+    log.info("Message payload as string: {}", json);
+    Event event = objectMapper.readValue(json, Event.class);
+    log.info("Message payload as object: {}", event);
+  }
+
+  public static String toText(byte[] bytes) throws UnsupportedEncodingException {
+    String text = new String(bytes, "UTF-8");
+    log.info("print: " + text);
+    return text;
   }
 }
